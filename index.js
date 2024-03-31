8d832e468e756b3f80f350afabae346451dbbff8
kaiyochanx3 <165566159+kaiyochanx3@users.noreply.github.com>
2024-03-31 17:30:39 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Yemek yiyor","Sıkılıyor"];
 
 
 let currentIndex = 0;
